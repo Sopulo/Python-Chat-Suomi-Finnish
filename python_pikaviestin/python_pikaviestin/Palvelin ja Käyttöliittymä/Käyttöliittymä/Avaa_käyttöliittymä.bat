8b84a977fed2bb29cec0ed4bf4840a1f13385f1f
Sopulo <106150898+Sopulo@users.noreply.github.com>
2022-11-23 12:09:02 +0200

    Add files via upload

---
+++ python_pikaviestin/python_pikaviestin/Palvelin ja Käyttöliittymä/Käyttöliittymä/Avaa_käyttöliittymä.bat
@@ -0,0 +1,13 @@
+@echo off
+color 4
+color 4
+color 4
+color 6
+color 6
+color 6
+echo Varmista etta olet asentanut ohjelman Python3
+color 6
+@echo off
+python3 client.py
+@echo off
+pause
---
+++ python_pikaviestin/python_pikaviestin/Palvelin ja Käyttöliittymä/Palvelin/Avaa_palvelin.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Varmista etta olet asentanut ohjelman Python3
+color 6
+@echo off
+python3 server.py
+@echo off
+pause
